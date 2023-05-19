08be0d7b7e15a861f074ffa82c27e93f0fe7d5c8
VincentB <vinc.brocheton@gmail.com>
2023-05-19 19:27:39 +0200

    first commit

---
+++ src/components/ReturnButton/ReturnButton.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {Keyboard, KeyboardReturn} from "@mui/icons-material";
+import {useNavigate} from "react-router-dom";
+
+const ReturnButton = ({link, text}) => {
+    const navigate = useNavigate();
+
+    const returnTo = () => {
+        navigate(link);
+    }
+    return (
+        <div className="return-button">
+            <div onClick={returnTo}>
+                <KeyboardReturn/>
+                <span>{text}</span>
+            </div>
+        </div>
+    );
+}
+
+export default ReturnButton;
---
+++ src/pages/AdminPage/AdminPanel.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "../styles/adminpanel.css"
+import {Link} from "react-router-dom";
+
+const AdminPanel = () => {
+    return (
+        <>
+            <h1>Admin Panel</h1>
+            <div className="content-admin">
+
+                <Link to={"#"} className="nav-link">
+                    <div className="button-menu">
+                        Gestion des utilisateurs
+                    </div>
+                </Link>
+                <Link to={"#"} className="nav-link">
+                    <div className="button-menu">Gestion des Races</div>
+                </Link>
+                <Link to={"#"} className="nav-link">
+                    <div className="button-menu">Gestion des classes</div>
+                </Link>
+                <Link to={"#"} className="nav-link">
+                    <div className="button-menu">Gestion des Sorts</div>
+                </Link>
+                <Link to={"#"} className="nav-link">
+                    <div className="button-menu">Gestion des Objets</div>
+                </Link>
+                <Link to={"#"} className="nav-link">
+                    <div className="button-menu">Gestion des Dons</div>
+                </Link>
+                <Link to={"#"} className="nav-link">
+                    <div className="button-menu">Gestion des Compétences</div>
+                </Link>
+                <Link to={"#"} className="nav-link">
+                    <div className="button-menu">Gestion des écoles</div>
+                </Link>
+                <Link to={"#"} className="nav-link">
+                    <div className="button-menu">Gestion des Manifestations Occultes</div>
+                </Link>
+            </div>
+        </>
+    );
+}
+
+export default AdminPanel
